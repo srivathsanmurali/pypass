0ed60f4c018fdd9245922ff6b95e686c2045f274
Srivathsan Murali <srivathsan92@gmail.com>
2016-07-30 09:19:32 +0200

    db update

--- database.py
+++ database.py
@@ -15,6 +15,7 @@ class Database(object):
     #self.filepath = filepath
     #self.entries = readDatabase(self.filepath);
     self.entries = {}
+    self.dbChanged = False
 
   def readDatabase(self, filepath):
     print "Reading database..."
@@ -26,6 +27,9 @@ class Database(object):
   def addEntry(self):
     print "New Entry..."
     name = raw_input("Entry name : ")
+    if not name:
+      print "name cant be blank"
+      return
     if self.entries.has_key(name):
       print "An entry with the name ", name ," exists already"
       return False
@@ -40,9 +44,48 @@ class Database(object):
     URL = raw_input("URL : ")
     newEntry = entry.Entry(name, username, password, "internet", URL)
     self.entries[name]= newEntry
+    self.dbChanged = True
+
     copyToClipboard = utils.choice("Copy password to clipboard? (Y/n)")
     print "\n"
+
     return True;
+
+  def editEntry(self, name):
+    if not self.entries.has_key(name):
+      print 'No entry with name ', name
+      return
+
+    print "Edit Entry ", name
+    print "Press enter to no edit field"
+
+    username = raw_input('Username : ')
+    if username == "":
+      username = self.entries[name].username
+
+    password = raw_input('Password : ')
+    if password == "":
+        genPass = utils.choice("Genrate password? (Y/n)")
+        if genPass == False:
+          password = self.entries[name].password
+        else:
+          password = passGen.getRandomPass()
+          print "Password : ", password
+
+    URL = raw_input('URL : ')
+    if URL == "":
+      URL = self.entries[name].URL
+
+    tag = "internet"
+    change = entry.Entry(name, username, password,tag, URL)
+
+    change.display(True)
+    modify = utils.choice("complete edit ? (Y/n)")
+    if modify:
+      self.entries[name].edit(change)
+      print "Editted entry ", name
+      self.dbChanged = True;
+
 
   def listEntries(self):
     print "Entries..."
@@ -60,16 +103,15 @@ class Database(object):
     if self.entries.has_key(name):
       print "Deleting Entry ", name ,"..."
       del self.entries[name]
+      seld.dbChanged = True;
     else:
       print "Cant find entry ", name , "!!"
 
 def test():
   db = Database()
   db.addEntry()
-  db.listEntries()
-  db.openEntry("fb")
+  db.editEntry("fb")
   db.delEntry("fb")
-  db.delEntry("bf")
 
 
 if __name__ == "__main__":
